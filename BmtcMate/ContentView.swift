ce039e9cb97be102ad1e2119c9ddbd23bf2e4f17
Shrish Deshpande <harrypotter.starwars.shrish@gmail.com>
2023-06-10 21:08:50 +0530

    It compiles

--- BmtcMate/ContentView.swift
+++ BmtcMate/ContentView.swift
@@ -8,21 +8,6 @@ struct ContentView: View {
             Spacer()
         }
     }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(timestamp: Date())
-            modelContext.insert(newItem)
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            for index in offsets {
-                modelContext.delete(items[index])
-            }
-        }
-    }
 }
 
 #Preview {
